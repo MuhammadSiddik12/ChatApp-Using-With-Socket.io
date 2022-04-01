32f5023b1ea2826a8e20e314fc5254e6f0f5946f
muhammad.siddik <muhhammad.siddik@team.cliffex.com>
2022-04-01 12:35:34 +0530

    add new thing

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(process.env.PORT, () => {
-    console.log(`server is running on port ${process.env.PORT}`);
+server.listen(process.env.PORT || 8000, () => {
+    console.log(`server is running on port ${process.env.PORT || 8000} `);
 })
